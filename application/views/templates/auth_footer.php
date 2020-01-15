6e89e2106748b078453647b82578f23e540dc085
sony17ekaananda <sony17ekaananda@gmail.com>
2020-01-15 13:12:45 +0700

    dasar admin dan user

---
+++ application/views/templates/auth_footer.php
@@ -0,0 +1,13 @@
+<!-- Bootstrap core JavaScript-->
+  <script src="<?php base_url() ?> assests/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php base_url() ?> assests/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?php base_url() ?> assests/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?php base_url() ?> assests/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
